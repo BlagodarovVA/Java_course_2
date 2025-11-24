da955253e0668e79b973214f8146b1f7b66d4469
Valery Blagodarov <11w-walerik@mail.ru>
2025-11-24 15:52:32 +0300

    3_generics done

--- src/main/java/generics_3/ParametrizedClass.java
+++ src/main/java/generics_3/ParametrizedClass.java
@@ -2,18 +2,25 @@ package generics_3;
 
 public class ParametrizedClass {
     public static void main(String[] args) {
-        Info<String> info1 = new Info<>("privet");
-        System.out.println(info1);
-        String s = info1.getValue();
-
-        Info<Integer> info2 = new Info<>(55);
-        System.out.println(info2);
-        Integer i1 = info2.getValue();
-
-        InfoExt<Double> info3 = new InfoExt<>(3.14);
-        System.out.println(info3);
-        Double d1 = info3.getValue();
+//        Info<String> info1 = new Info<>("privet");
+//        System.out.println(info1);
+//        String s = info1.getValue();
+//
+//        Info<Integer> info2 = new Info<>(55);
+//        System.out.println(info2);
+//        Integer i1 = info2.getValue();
+//
+//        InfoExt<Double> info3 = new InfoExt<>(3.14);
+//        System.out.println(info3);
+//        Double d1 = info3.getValue();
     }
+
+//    public void abc(Info<String> info){     // для jvm эти 2 метода одинаковы - abc(Info info), поэтому вместе не могут работать
+//        String s = info.getValue();
+//    }
+//    public void abc(Info<Integer> info){
+//        Integer i = info.getValue();
+//    }
 }
 
 class Info <T>{                 // T - type placeholder
@@ -55,6 +62,17 @@ interface I1{}
 
 interface I2{}
 
+class Parent{
+    public void abc(Info<String> info){
+        String s = info.getValue();
+    }
+}
 
+class Child extends Parent{
+    // так тоже нельзя оверрайдить, т.к. при компиляции стирается инфа о дженериках
+//    public void abc(Info<Integer> info){
+//        Integer i = info.getValue();
+//    }
+}
 
 
--- src/main/java/generics_3/game/Test.java
+++ src/main/java/generics_3/game/Test.java
@@ -4,37 +4,40 @@ public class Test {
     public static void main(String[] args) {
         Schoolar schoolar1 = new Schoolar("Alex", 16);
         Schoolar schoolar2 = new Schoolar("Katya", 15);
+        Schoolar schoolar3 = new Schoolar("Petr", 14);
+        Schoolar schoolar4 = new Schoolar("Alena", 13);
 
         Student student1 = new Student("Vadim", 20);
         Student student2 = new Student("Tatiana", 21);
+        Student student3 = new Student("Andrey", 22);
+        Student student4 = new Student("Elena", 23);
 
         Employee employee1 = new Employee("Valery", 35);
         Employee employee2 = new Employee("Diana", 28);
 
-        Team<Schoolar> schoolarTeam = new Team <>("Драконы");
-        schoolarTeam.addNewParticipant(schoolar1);
-        schoolarTeam.addNewParticipant(schoolar2);
+        Team<Schoolar> schoolarTeam1 = new Team <>("Драконы");
+        schoolarTeam1.addNewParticipant(schoolar1);
+        schoolarTeam1.addNewParticipant(schoolar2);
+        Team<Schoolar> schoolarTeam2 = new Team <>("Малакасосы");
+        schoolarTeam2.addNewParticipant(schoolar3);
+        schoolarTeam2.addNewParticipant(schoolar4);
 
-        Team<Student> studentTeam = new Team <>("KOK");
-        studentTeam.addNewParticipant(student1);
-        studentTeam.addNewParticipant(student2);
+        Team<Student> studentTeam1 = new Team <>("Трусы");
+        studentTeam1.addNewParticipant(student1);
+        studentTeam1.addNewParticipant(student2);
+        Team<Student> studentTeam2 = new Team <>("Балбесы");
+        studentTeam2.addNewParticipant(student3);
+        studentTeam2.addNewParticipant(student4);
 
-        Team<Employee> employeeTeam = new Team <>("StarPer");
+        Team<Employee> employeeTeam = new Team <>("Starper");
         employeeTeam.addNewParticipant(employee1);
         employeeTeam.addNewParticipant(employee2);
 
 //        Team<String> otherTeam = new Team <>("Other");
 
-        Team<Schoolar> schoolarTeam2 = new Team <>("Малакасосы");
-        Schoolar schoolar3 = new Schoolar("Petr", 14);
-        Schoolar schoolar4 = new Schoolar("Alena", 13);
-        schoolarTeam2.addNewParticipant(schoolar3);
-        schoolarTeam2.addNewParticipant(schoolar4);
-
         System.out.println("----------");
-        schoolarTeam.playWith(schoolarTeam2);
-
-
+        schoolarTeam1.playWith(schoolarTeam2);
+        studentTeam1.playWith(studentTeam2);
 
     }
 }
