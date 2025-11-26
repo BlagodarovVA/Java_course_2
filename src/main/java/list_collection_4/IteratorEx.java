bf34bf8d535ae9d8eba108ed7cef781cdfb43ec4
Valery Blagodarov <11w-walerik@mail.ru>
2025-11-26 16:17:47 +0300

    4.6 done

---
+++ src/main/java/list_collection_4/IteratorEx.java
@@ -0,0 +1,27 @@
+package list_collection_4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorEx {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("Valery");
+        arrayList1.add("Diana");
+        arrayList1.add("Marina");
+        arrayList1.add("Georg");
+        arrayList1.add("Diana");
+        System.out.println("arrayList1: " + arrayList1);
+
+        Iterator<String> iterator = arrayList1.iterator();
+        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//            iterator.next();            // получаем элемент
+
+            if(iterator.next() == "Marina"){
+                iterator.remove();          // удаляем элемент
+            }
+        }
+        System.out.println("arrayList1: " + arrayList1);
+    }
+}
