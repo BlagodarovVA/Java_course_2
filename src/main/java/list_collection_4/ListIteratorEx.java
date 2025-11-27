b4e22679a4d7dbd83fcd618e66e8ccddcba8c5f6
Valery Blagodarov <11w-walerik@mail.ru>
2025-11-27 14:31:05 +0300

    4.8 done

---
+++ src/main/java/list_collection_4/ListIteratorEx.java
@@ -0,0 +1,28 @@
+package list_collection_4;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListIteratorEx {
+    public static void main(String[] args) {
+
+        String s = "madam";
+        List<Character> list = new LinkedList<>();
+        for (char ch : s.toCharArray()){
+            list.add(ch);
+        }
+        System.out.println(list);
+
+        ListIterator<Character> iterator = list.listIterator();
+        ListIterator<Character> reversIterator = list.listIterator(list.size());
+        boolean isPalindrome = true;
+        while (iterator.hasNext() && reversIterator.hasPrevious()){
+            if(iterator.next() != reversIterator.previous()){
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("String '" + s +  "' is Palindrome: " + isPalindrome);
+    }
+}
