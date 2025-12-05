b5af313e8c1436f4d39f9650da2a07ac631e0377
Valery Blagodarov <11w-walerik@mail.ru>
2025-12-05 16:23:27 +0300

    4.15 done

---
+++ src/main/java/list_collection_4/StackEx2.java
@@ -0,0 +1,33 @@
+package list_collection_4;
+
+import java.util.Stack;
+
+public class StackEx2 {
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+        stack.push("Valery");
+        stack.push("Diana");
+        stack.push("Misha");
+        stack.push("Katya");
+        System.out.println(stack);
+
+        System.out.println(stack.pop());    // возвращаем последний элемент и удаляем
+        System.out.println(stack);
+
+        // если стек пуст, то будет исключение, поэтому проверка
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+            System.out.println(stack);
+
+        }
+
+        stack.push("Valery");
+        stack.push("Diana");
+        stack.push("Misha");
+        stack.push("Katya");
+        System.out.println(stack.peek());       // вывод верхнего (последнего) элемента без удаления
+        System.out.println(stack);
+
+    }
+}
