c8699eda58afd35008201189e24ec95697d18900
Valery Blagodarov <11w-walerik@mail.ru>
2025-12-05 15:53:35 +0300

    4.11 done

---
+++ src/main/java/list_collection_4/VectorEx.java
@@ -0,0 +1,20 @@
+package list_collection_4;
+
+import java.util.Vector;
+
+public class VectorEx {
+    public static void main(String[] args) {
+
+        Vector<String> vector = new Vector<>();
+        vector.add("Valery");
+        vector.add("Diana");
+        vector.add("Katya");
+        vector.add("Mihail");
+        System.out.println(vector);
+        System.out.println(vector.firstElement());
+        System.out.println(vector.lastElement());
+        vector.remove(3);
+        System.out.println(vector);
+        System.out.println(vector.get(1));
+    }
+}
