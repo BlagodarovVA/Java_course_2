4e1c80afa2d8d9f42d1abb4eec1670fccbd16252
Valery Blagodarov <11w-walerik@mail.ru>
2025-12-19 16:33:43 +0300

    8.3 done

---
+++ src/main/java/multithreading_8/Ex6.java
@@ -0,0 +1,15 @@
+package multithreading_8;
+
+public class Ex6 implements Runnable{
+    public void run() {
+        System.out.println("Method run. Thread name = " +
+                Thread.currentThread().getName());        // текущий поток
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Ex6());
+        thread.start();
+        System.out.println("Method main. Thread name = " +
+                Thread.currentThread().getName());
+    }
+}
