c5de773bed9b78a220863ebea108e2812b4df29a
Valery Blagodarov <11w-walerik@mail.ru>
2025-12-22 13:00:21 +0300

    8.7 done

---
+++ src/main/java/r8_multithreading/Ex11.java
@@ -0,0 +1,31 @@
+package r8_multithreading;
+
+public class Ex11 {
+    static int counter = 0;
+    // synchronized ставит блокировку, одновременно сможет выполнять метод только 1 поток
+    public static synchronized void increment(){
+        counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new R());
+        Thread thread2 = new Thread(new R());
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        System.out.println(counter);
+    }
+}
+
+
+class R implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            Ex11.increment();
+        }
+    }
+}
