0527a04ee831247ad75b57a0bdcdb947e8d13e8d
Valery Blagodarov <11w-walerik@mail.ru>
2026-01-08 14:41:05 +0300

    8.12 done

---
+++ src/main/java/r8_multithreading/DeamonEx.java
@@ -0,0 +1,48 @@
+package r8_multithreading;
+
+public class DeamonEx {
+    public static void main(String[] args) {
+        System.out.println("Main thread starts");
+        UserThread userThread = new UserThread();
+        userThread.setName("user_thread");
+        DaemonThread daemonThread = new DaemonThread();
+        daemonThread.setName("daemon_thread");
+        daemonThread.setDaemon(true);   // этот поток завершится, как только завершится userThread
+        userThread.start();
+        daemonThread.start();
+
+
+        System.out.println("Main thread ends");
+    }
+}
+
+
+class UserThread extends Thread{
+    public void run(){
+        System.out.println(Thread.currentThread().getName() + " is deamon: " + isDaemon());
+
+        for (char i = 'A'; i <= 'J'; i++){
+            try {
+                sleep(300);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class DaemonThread extends Thread{
+    public void run(){
+        System.out.println(Thread.currentThread().getName() + " is deamon: " + isDaemon());
+
+        for (int i = 1; i <= 1000; i++){
+            try {
+                sleep(100);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
