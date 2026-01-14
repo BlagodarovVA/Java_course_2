f31e9bb20a1f0d9777dd91f54659eb13969645b7
Valery Blagodarov <11w-walerik@mail.ru>
2026-01-14 12:33:31 +0300

    8.24 done

---
+++ src/main/java/r4_collection/thread_safe/CopyOnWriteArrayListEx.java
@@ -0,0 +1,46 @@
+package r4_collection.thread_safe;
+
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListEx {
+    public static void main(String[] args) throws InterruptedException {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("Valery");
+        list.add("Diana");
+        list.add("Misha");
+        list.add("Olya");
+        list.add("Petr");
+        System.out.println(list);
+
+        Runnable runnable1 = () -> {
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(iterator.next());
+            }
+        };
+
+        Runnable runnable2 = () -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list.remove(4);
+            list.add("Yuri");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(list);
+    }
+}
